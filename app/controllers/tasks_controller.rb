f286545e3b1ec95660ef7c66f355ac9824719591
/code/$GITHUB_USERNAME/dotfiles <diego.delaminne@gmail.com>
2018-10-30 18:02:15 +0100

    rails task manger

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(user_params)
+    @task.save
+    redirect_to tasks_path
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(user_params)
+    redirect_to tasks_path
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+
+  end
+
+  private
+
+  def user_params
+    params.require(:task).permit(:title, :details)
+  end
+end
